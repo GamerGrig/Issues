dfd446f98b9ad8500a7fbff899ce9d268734103f
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-28 16:57:50 +0300

    Add files via upload

---
+++ StartHW1.py
@@ -0,0 +1,18 @@
+a = 'Почините платформу!'
+print(len(a))
+
+first = 15
+second = 27
+summa = first + second
+diff = first - second
+print(summa, diff)
+third = 9
+mean = (first + second + third) / 3
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string, ',', first_string)
+a = 14
+b = 65
+c = 85
+f = (a * b) + (a * c) ** 3 / 2
+print(f)
