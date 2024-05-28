f879d11e911e70d6e2b575165a3b2a882872dec6
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-28 17:32:29 +0300

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,5 @@
+homeworks = 12
+time_spent = 1.5
+course = 'Python'
+time_task = time_spent / homeworks
+print(f'Курс: {course}, всего задач:{homeworks}, затрачено часов: {time_spent}, среднее время выполнения {time_task} часа.')
