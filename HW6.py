07e9df2c3455b53df965d3bf1fbba4e2a3fcf808
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-28 20:05:49 +0300

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,17 @@
+my_dict = {'Кирилл': 20}
+print(my_dict)
+print(my_dict['Кирилл'])
+my_dict['Олег'] = 30
+print(my_dict['Олег'])
+my_dict.update({'Нина': 21, 'Ладо': 24})
+print(my_dict)
+my_dict.pop('Ладо')
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 4, 4, 4, True, False, True, 'bibop', 'pop', 'pop'}
+print(my_dict)
+my_set.add('bro')
+my_set.add(5)
+my_set.discard(4)
+print(my_set)
+
