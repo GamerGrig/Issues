7f00e57bacf4bcc80d4eaf44bd1c5ea557995a68
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-10-02 15:48:31 +0500

    Update module 6_2.py

--- module 6_2.py
+++ module 6_2.py
@@ -1,41 +1,52 @@
 class Vehicle:
-
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, __model, __engine_power, __color):
-        self.owner = str(owner)
-        self.__model = str(__model)
-        self.__engine_power = int(__engine_power)
-        self.__color = __color
+    def __init__(self, owner: str, model: str, engine_power: int, color: str):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = self.set_color(color)
 
     def get_model(self):
-        print(f'Модель: {self.__model} ')
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power} ')
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        print(f'Цвет: {self.__color} ')
-
-    def print_info(self):
-        print(f'Модель: {self.__model} ')
-        print(f'Мощность двигателя: {self.__engine_power} ')
-        print(f'Цвет: {self.__color} ')
-        print(f'Владелец: {self.owner} ')
+        return f'Цвет: {self.__color}'
 
     def set_color(self, new_color):
-        if str.lower(new_color) not in Vehicle.__COLOR_VARIANTS:
-            print(f'Нельзя сменить цвет на {new_color}')
-        self.__color = new_color
+        if new_color.lower() not in self.__COLOR_VARIANTS:
+            print(f'Нельзя сменить цвет на: {new_color}')
+        else:
+            self.__color = new_color
+            return self.__color
+
+    def print_info(self):
+        return f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}\n'
 
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
+    def __init__(self, owner, model, engine_power, color, limit):
+        super().__init__(owner, model, engine_power, color)
+        self.limit = self.sedan_passengers(limit)
 
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
-vehicle1.print_info()
+    def sedan_passengers(self, limit):
+        if Sedan.__PASSENGERS_LIMIT != limit:
+            return f'В седане только 5 мест'
+        else:
+            self.limit = limit
+            return self.limit
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue', 4)
+print(vehicle1.print_info())
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
-vehicle1.print_info()
+print(vehicle1.print_info())
+print('*' * 20)
+print(vehicle1.limit)
